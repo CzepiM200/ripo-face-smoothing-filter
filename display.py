fce4c5925c61e60e65c55a5e598842297d1a87a3
Xavier Davis <mlodybartmann@gmail.com>
2020-05-23 20:10:16 +0200

    Wykrywanie twarzy, przycisk do włączania filtru

--- display.py
+++ display.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
 
 import cv2 as cv
+#Import funkcji wł/wył filtr
+from smoothing_filter import toggle
 
 #Utworzenie okna
-cv.namedWindow("Wygładzanie twarzy")
+cv.namedWindow("Face smoothing")
 
 #Pobranie feedu z kamery
-feed =  cv.VideoCapture(0)
+feed = cv.VideoCapture(0)
 
+#Załadowaine klasyfikatora
+face_detection = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+#Przełącznik filtru
+cv.createTrackbar("Filtr", "Face smoothing", 0, 1, toggle)
+
+#Czytanie obrazu z kamery dopóki jest dostępna
 if feed.isOpened():
     opened, frame = feed.read()
 else:
     opened = False
 
+#Dopóki obraz z kamery jest wczytywany, wyświetlanie go
 while opened:
-    cv.imshow("Wygładzanie twarzy", frame)
     opened, frame = feed.read()
+    #Konwersja na skalę szarości - pozwala na wykrycie twarzy
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_detection.detectMultiScale(gray, minNeighbors=6, minSize=(100, 100))
+    for (x, y, width, height) in faces:
+        cv.rectangle(frame, (x, y), (x + width, y + width), (0xDD, 0x77, 0xAA), 2)
+
+    #Wyświetlenie obrazu w oknie o wysokości 800px
+    frame = cv.resize(frame, (int(frame.shape[1] / frame.shape[0] * 800), 800))
+    cv.imshow("Face smoothing", frame)
+    #Zamknięcie po wciśnięciu klawisza ESC
     key = cv.waitKey(20)
     if key == 27:
         break
 
-cv.destroyWindow("Wygładzanie twarzy")
+#Zamknięcie okna
+cv.destroyWindow("Face smoothing")
+#Zwolnienie dostępu do kamery
 feed.release()
---
+++ smoothing_filter.py
@@ -0,0 +1,14 @@
+
+def toggle(x):
+    if x == 0:
+        enable()
+    else:
+        disable()
+
+def enable():
+    #Zawartosc funkcji włączającej filtr
+    return
+
+def disable():
+    #Zawartość funkcji wyłączającej filtr
+    return
