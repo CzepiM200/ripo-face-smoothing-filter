4525bcaae0634867cd613961714cb219afd6a200
Marcin Czepiela <czepimm200@gmail.com>
2020-05-27 17:20:22 +0200

    Wykrywanie oczu

--- display.py
+++ display.py
@@ -3,7 +3,7 @@
 import cv2 as cv
 #Import funkcji wł/wył filtr
 from smoothing_filter import toggle
-
+import numpy as np
 #Utworzenie okna
 cv.namedWindow("Face smoothing")
 
@@ -12,6 +12,8 @@ feed = cv.VideoCapture(0)
 
 #Załadowaine klasyfikatora
 face_detection = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+eyes_detection = cv.CascadeClassifier("haarcascade_eye.xml")
+#mouth_detection = cv.CascadeClassifier(cv.data.haarcascades + "Mouth.xml")
 
 #Przełącznik filtru
 cv.createTrackbar("Filtr", "Face smoothing", 0, 1, toggle)
@@ -25,15 +27,42 @@ else:
 #Dopóki obraz z kamery jest wczytywany, wyświetlanie go
 while opened:
     opened, frame = feed.read()
+    mainFrame = frame.copy()
+
     #Konwersja na skalę szarości - pozwala na wykrycie twarzy
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_detection.detectMultiScale(gray, minNeighbors=6, minSize=(100, 100))
-    for (x, y, width, height) in faces:
-        cv.rectangle(frame, (x, y), (x + width, y + width), (0xDD, 0x77, 0xAA), 2)
+    faces = face_detection.detectMultiScale(gray, 1.3, 5)
+    
 
-    #Wyświetlenie obrazu w oknie o wysokości 800px
-    frame = cv.resize(frame, (int(frame.shape[1] / frame.shape[0] * 800), 800))
-    cv.imshow("Face smoothing", frame)
+    #Rysowanie kwadratu na wykrytej twarzy
+    for (x, y, width, height) in faces:
+        cv.rectangle(frame, (x, y), (x + width, y + width), (0xD9, 0x67, 0x04), 2)
+        face_color = frame[y:y+height, x:x+width]
+        
+        #twarz
+        mainFace = frame[y:y+height, x:x+width].copy()
+        
+        #wykrywanie oczu
+        eye_gray = gray[y:y+height, x:x+width]
+        eye_color = frame[y:y+height, x:x+width]
+        eyes = eyes_detection.detectMultiScale(eye_gray)
+        for (eye_x, eye_y, eye_width, eye_height) in eyes:
+            cv.rectangle(eye_color, (eye_x, eye_y), (eye_x+eye_width, eye_y+eye_height), (0xD9, 0xA2, 0x3D), 1)
+            
+            #nakładanie filtru na twarz
+            kernel = np.ones((5,5),np.float32)/25
+            mainFace = cv.filter2D(eye_color,-1,kernel)
+
+            #nakładanie oczu bez filtru na twarz
+            mainFace[eye_y:eye_y+eye_height, eye_x:eye_x+eye_width] = eye_color[eye_y:eye_y+eye_height, eye_x:eye_x+eye_width].copy()
+            mainFrame[y:y+height, x:x+width] = mainFace.copy()
+
+    kernel = np.ones((5,5),np.float32)/25
+    mainFrame = cv.filter2D(mainFrame,-1,kernel)
+
+    mainFrame = cv.resize(mainFrame, (int(mainFrame.shape[1] / mainFrame.shape[0] * 800), 800))
+    cv.imshow("Face smoothing", mainFrame)
+
     #Zamknięcie po wciśnięciu klawisza ESC
     key = cv.waitKey(20)
     if key == 27:
