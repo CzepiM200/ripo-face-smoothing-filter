bf2297b67f623cdad0429261cc0d0d9fe5cf9b76
Marcin Czepiela <czepimm200@gmail.com>
2020-05-30 18:41:10 +0200

    Add mouth predictor

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ import dlib
 # 	help="./ibug_300W_large_face_landmark_dataset/mouth_predictor.dat")
 # args = vars(ap.parse_args())
 
-print("[INFO] loading facial landmark predictor...")
+#Wczytywanie prekyktorów oczy i ust
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('./eye_predictor.dat')
-#mouthPredictor = dlib.shape_predictor('./ibug_300W_large_face_landmark_dataset/mouth_predictor.dat')
-
+eyesPredictor = dlib.shape_predictor('./eye_predictor.dat')
+mouthPredictor = dlib.shape_predictor('./mouth_predictor.dat')
 
 #Zmienna określająca stan włączenia filtru
 filter_enabled = 0
@@ -35,9 +34,6 @@ cv.namedWindow("Face smoothing")
 #Pobranie feedu z kamery
 feed = cv.VideoCapture(0)
 
-#Załadowanie klasyfikatora
-face_detection = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
-
 #Przełącznik filtru
 cv.createTrackbar("Filtr", "Face smoothing", 0, 1, toggle)
 
@@ -50,13 +46,13 @@ else:
 #Dopóki obraz z kamery jest wczytywany, wyświetlanie go
 while opened:
     opened, frame = feed.read()
-	#frame = imutils.resize(frame, width=800)
 
     #Konwersja na skalę szarości - pozwala na wykrycie twarzy
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #faces = face_detection.detectMultiScale(gray, minSize=(100, 100))
     
     frame = imutils.resize(frame, width=640)
+
+    #Detekcja twarzy
     rects = detector(gray, 0)
 
     #Rysowanie kwadratu na wykrytej twarzy
@@ -64,11 +60,19 @@ while opened:
         (x, y, w, h) = face_utils.rect_to_bb(rect)
         cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+        #Detekja oczu
+        eyes = eyesPredictor(gray, rect)
+        eyes = face_utils.shape_to_np(eyes)
 		
-        for (sX, sY) in shape:
+        for (sX, sY) in eyes:
             cv.circle(frame, (sX, sY), 1, (0, 0, 255), -1)
+        
+        #Detekcja ust
+        mouths = mouthPredictor(gray, rect)
+        mouths = face_utils.shape_to_np(mouths)
+		
+        for (mX, mY) in mouths:
+            cv.circle(frame, (mX, mY), 1, (255, 0, 0), -1)
 
     #if filter_enabled == 1:
         #Tu miejsce na filtrację
