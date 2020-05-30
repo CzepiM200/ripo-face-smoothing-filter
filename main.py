70da582728b7d368b9192c28f816c9af5613f6ed
Marcin Czepiela <czepimm200@gmail.com>
2020-05-30 17:26:49 +0200

    Add eye predictor

--- main.py
+++ main.py
@@ -2,6 +2,23 @@
 
 import cv2 as cv
 import numpy as np
+from imutils import face_utils
+import argparse
+import imutils
+import time
+import dlib
+
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-p", "--shape-epredictor", required=True,
+# 	help="./ibug_300W_large_face_landmark_dataset/eye_predictor.dat")
+# ap.add_argument("-p", "--shape-mpredictor", required=True,
+# 	help="./ibug_300W_large_face_landmark_dataset/mouth_predictor.dat")
+# args = vars(ap.parse_args())
+
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('./eye_predictor.dat')
+#mouthPredictor = dlib.shape_predictor('./ibug_300W_large_face_landmark_dataset/mouth_predictor.dat')
 
 
 #Zmienna określająca stan włączenia filtru
@@ -33,20 +50,31 @@ else:
 #Dopóki obraz z kamery jest wczytywany, wyświetlanie go
 while opened:
     opened, frame = feed.read()
+	#frame = imutils.resize(frame, width=800)
 
     #Konwersja na skalę szarości - pozwala na wykrycie twarzy
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_detection.detectMultiScale(gray, minSize=(100, 100))
+    #faces = face_detection.detectMultiScale(gray, minSize=(100, 100))
+    
+    frame = imutils.resize(frame, width=640)
+    rects = detector(gray, 0)
 
     #Rysowanie kwadratu na wykrytej twarzy
-    for (x, y, width, height) in faces:
-        cv.rectangle(frame, (x, y), (x + width, y + height), (0xD9, 0x67, 0x04), 2)
+    for rect in rects:
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+		
+        for (sX, sY) in shape:
+            cv.circle(frame, (sX, sY), 1, (0, 0, 255), -1)
 
     #if filter_enabled == 1:
         #Tu miejsce na filtrację
 
 
-    frame = cv.resize(frame, (int(frame.shape[1] / frame.shape[0] * 800), 800))
+    #frame = cv.resize(frame, (int(frame.shape[1] / frame.shape[0] * 800), 800))
     cv.imshow("Face smoothing", frame)
 
     #Zamknięcie po wciśnięciu klawisza ESC
